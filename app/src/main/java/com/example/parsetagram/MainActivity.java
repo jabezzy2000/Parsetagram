d46cd0f1aa43958d38e78b01f8ef95c9e210e843
Jabez Agyemang-Prempeh <jabezap@fb.com>
2022-06-14 16:55:39 -0700

    Linked Parse database Successful interaction between application and database Successfully implemented implicit intent to use camera and back

--- app/src/main/java/com/example/parsetagram/MainActivity.java
+++ app/src/main/java/com/example/parsetagram/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.parsetagram;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,17 +20,26 @@ import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+    private File photoFile;
+    private String photoFileName = "photo.jpg";
+    private String fileName = "photo.jpg";
     Button btn;
     Button btnPicture;
     EditText etDescription;
     ImageView ivPostImage;
+    Button btnCaptureImg;
     String TAG = "tag";
+
 
 
 
@@ -33,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_activity);
         btn = findViewById(R.id.btnLogOut);
         btnPicture = findViewById(R.id.btnPicture);
+        btnCaptureImg = findViewById(R.id.btnCaptureImg);
         etDescription = findViewById(R.id.etDescription);
         ivPostImage = findViewById(R.id.ivPostImage);
 
@@ -46,10 +63,111 @@ public class MainActivity extends AppCompatActivity {
                 logout();
             }
         });
+
+        btnPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"submit picture button was clicked", Toast.LENGTH_SHORT).show();
+                String description = etDescription.getText().toString();
+                if (description.isEmpty()) {
+                    Toast.makeText(MainActivity.this,"Description left empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (photoFile == null || ivPostImage.getDrawable()== null ) {
+                    Toast.makeText(MainActivity.this,"No Image attached", Toast.LENGTH_SHORT).show();
+                }
+                ParseUser currentUser =ParseUser.getCurrentUser();
+                savePost(description,currentUser, photoFile);
+            }
+        });
+
+        btnCaptureImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"capture image button was clicked", Toast.LENGTH_SHORT).show();
+                launchCamera();
+
+            }
+        });
+    }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(MainActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ivPostImage.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private File getPhotoFileUri(String photoFileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        return (new File(mediaStorageDir.getPath() + File.separator + fileName));
+
+
+    }
+
+
+    private void savePost(String description, ParseUser currentUser, File photoFile) {
+        Post post = new Post();
+        post.setDescription(description);
+        post.setImage(new ParseFile(photoFile));
+        post.setUser(currentUser);
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if( e!= null) {
+                    Log.e(TAG, "error: Something went wrong with save in background method ", e );
+                    Toast.makeText(MainActivity.this,"Error trying to save post", Toast.LENGTH_SHORT).show();
+                }
+                Log.i(TAG, "done: Post was successfull");
+                etDescription.setText("");
+                ivPostImage.setImageResource(0);
+
+
+            }
+        });
     }
 
     private void queryPosts() {
-        ParseQuery<Post> query = null;
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
         query.include(Post.KEY_USER);
         query = ParseQuery.getQuery(Post.class);
         query.findInBackground(new FindCallback<Post>() {
